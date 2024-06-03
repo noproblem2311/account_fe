6301d56f695af4bf8396c55c8028e67d05a6e131
noproblem2311 <quanpva.dev@gmail.com>
2024-06-03 10:35:10 +0700

    update table

--- src/app/(platform)/_component/Table/TableHistory.jsx
+++ src/app/(platform)/_component/Table/TableHistory.jsx
@@ -9,9 +9,16 @@ const columns = [
     key: 'timePeriod',
   },
   {
+    title: 'process',
+    dataIndex: 'process',
+    key: 'process',
+  },
+  {
     title: 'Loại giấy tờ',
     dataIndex: 'documentType',
     key: 'documentType',
+    render: (text) => <span dangerouslySetInnerHTML={{__html: text.join('<br>')}} />,
+
   },
   {
     title: 'Trạng thái',
@@ -23,16 +30,50 @@ const columns = [
     title: 'Thông tin',
     dataIndex: 'information',
     key: 'information',
+    render: (text) => {
+      let info;
+      try {
+        text = text.replace(/'/g, '"');
+        text = text.replace(/^"/, "").replace(/"$/, "");
+        console.log("text",text);
+
+        info = JSON.parse(text);
+      } catch (e) {
+        console.error("Invalid JSON string:", e);
+        return <p>{text}</p>;
+      }
+  
+      if (Array.isArray(info)) {
+        return (
+          <div>
+            {info.map((item, index) => (
+              <div key={index} style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '10px' }}>
+                {Object.keys(item).map((key, idx) => (
+                  <p key={idx}><strong>{key}:</strong> {item[key]}</p>
+                ))}
+              </div>
+            ))}
+          </div>
+        );
+      } else {
+        return <p>Invalid JSON format</p>;
+      }
+    }
+    
+    
+    
   },
 ];
+
 const key_map = {
   "process_1": ["BẢNG CÂN ĐỐI TÀI KHOẢN"],
-  "process_2": ["THÔNG TƯ","BẢNG CÂN ĐỐI TÀI KHOẢN"],
-  "process_3": ["BẢNG CÂN ĐỐI TÀI KHOẢN","NHẬT KÝ CHUNG"],
-  "process_4": ["BÁO CÁO TÀI CHÍNH","NHẬT KÝ CHUNG"],
-  "process_5": ["TỜ KHAI QUYẾT TOÁN THUẾ THU NHẬP DOANH NGHIỆP","NHẬT KÝ CHUNG"],
-  "process_6": ["TỜ KHAI THUẾ GIÁ TRỊ GIA TĂNG","NHẬT KÝ CHUNG"],
+  "process_2": ["THÔNG TƯ", "BẢNG CÂN ĐỐI TÀI KHOẢN"],
+  "process_3": ["BẢNG CÂN ĐỐI TÀI KHOẢN", "NHẬT KÝ CHUNG"],
+  "process_4": ["BÁO CÁO TÀI CHÍNH", "NHẬT KÝ CHUNG"],
+  "process_5": ["TỜ KHAI QUYẾT TOÁN THUẾ THU NHẬP DOANH NGHIỆP", "NHẬT KÝ CHUNG"],
+  "process_6": ["TỜ KHAI THUẾ GIÁ TRỊ GIA TĂNG", "NHẬT KÝ CHUNG"],
 }
+
 const TableHistory = ({ datas = [] }) => {
   const datatable = [];
   for (const key in datas) {
@@ -42,8 +83,9 @@ const TableHistory = ({ datas = [] }) => {
       key: key,
       timePeriod: dataonbj['created_at'],
       status: dataonbj['status'],
+      process: dataonbj['process'],
       documentType: key_map[dataonbj['process']],
-      information: dataonbj['description'],
+      information: JSON.stringify(dataonbj['description']),
     };
 
     datatable.push(preobj);
@@ -51,6 +93,5 @@ const TableHistory = ({ datas = [] }) => {
 
   return <Table columns={columns} dataSource={datatable} />;
 };
-
 
 export default TableHistory;

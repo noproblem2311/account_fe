f10257c3e413de23f52c2323621a30c94a6d088b
noproblem2311 <quanpva.dev@gmail.com>
2024-06-03 13:53:46 +0700

    update table

--- src/app/(platform)/_component/HandleFile/HandleListFile.js
+++ src/app/(platform)/_component/HandleFile/HandleListFile.js
@@ -2,26 +2,29 @@
 import * as XLSX from 'xlsx/xlsx.mjs';
 import * as xml2js from "xml2js"; // Import xml2js library
 import exceljs from 'exceljs';
+import { toast } from 'react-hot-toast'; // Import toast from react-hot-toast
 
 const key_for_process = {
-  "nhật ký chung": [ "process_4","process_3","process_5","process_6"],
-  "BẢNG CÂN ĐỐI TÀI KHOẢN": ["process_1", "process_3","process_2"],
+  "nhật ký chung": ["process_4", "process_3", "process_5", "process_6"],
+  "BẢNG CÂN ĐỐI TÀI KHOẢN": ["process_1", "process_3", "process_2"],
   "thông tư 133": ["process_2"],
   "thông tư 200": ["process_2"],
   "báo cáo tài chính": ["process_4"],
   "TỜ KHAI QUYẾT TOÁN THUẾ THU NHẬP DOANH NGHIỆP ": ["process_5"],
   "TỜ KHAI THUẾ GIÁ TRỊ GIA TĂNG": ["process_6"],
 };
-const template={
-  "nhật ký chung":"NKC",
-  "BẢNG CÂN ĐỐI TÀI KHOẢN":"CDPS",
-  "thông tư 133":"TT133",
-  "thông tư 200":"TT200",
-  "báo cáo tài chính":"BCTC",
-  "TỜ KHAI QUYẾT TOÁN THUẾ THU NHẬP DOANH NGHIỆP ":"QTTN",
-  "TỜ KHAI THUẾ GIÁ TRỊ GIA TĂNG":"TTGT",
-}
-export   const  HandleListFile = async (userid, listfile) => {
+
+const template = {
+  "nhật ký chung": "NKC",
+  "BẢNG CÂN ĐỐI TÀI KHOẢN": "CDPS",
+  "thông tư 133": "TT133",
+  "thông tư 200": "TT200",
+  "báo cáo tài chính": "BCTC",
+  "TỜ KHAI QUYẾT TOÁN THUẾ THU NHẬP DOANH NGHIỆP ": "QTTN",
+  "TỜ KHAI THUẾ GIÁ TRỊ GIA TĂNG": "TTGT",
+};
+
+export const HandleListFile = async (userid, listfile) => {
   const list_file_after_check = {
     "listfile": [],
     "process_1": [],
@@ -33,72 +36,81 @@ export   const  HandleListFile = async (userid, listfile) => {
   };
 
   const baseUrl = `https://accountfolderpublic.s3.ap-southeast-1.amazonaws.com/${userid}/`;
+
   for (const file of listfile) {
-    const fileurl= baseUrl+file.name;
+    const fileurl = baseUrl + file.name;
     list_file_after_check["listfile"].push(fileurl);
   }
+
+  let foundTT133 = false;
+  let foundTT200 = false;
+
   for (const file of listfile) {
-    for (const [key, list_process] of Object.entries(key_for_process))
-      {
-        const is_key_in_file = await check_text_include_in_file(file,key);
-        if (is_key_in_file["status"]) {
-          for (const process of list_process) {
-            const pre_data={
-              "type": is_key_in_file["file"],
-              "name":file.name,
-            }
-            list_file_after_check[process].push(pre_data);
-          }
+    for (const [key, list_process] of Object.entries(key_for_process)) {
+      const is_key_in_file = await check_text_include_in_file(file, key);
+      if (is_key_in_file["status"]) {
+        if (key === "thông tư 133") {
+          foundTT133 = true;
+        }
+        if (key === "thông tư 200") {
+          foundTT200 = true;
+        }
+        for (const process of list_process) {
+          const pre_data = {
+            "type": is_key_in_file["file"],
+            "name": file.name,
+          };
+          list_file_after_check[process].push(pre_data);
         }
       }
+    }
   }
 
-  async function check_text_include_in_file(file, text) {
+  if (foundTT133 && foundTT200) {
+    toast.error("Chỉ được chọn một trong hai: Thông tư 133 hoặc Thông tư 200.");
+    return null; // or handle this case as per your application's requirements
+  }
 
-    // Read file content based on extension (assuming UTF-8 encoding):
-    let is_text_found = {"status":false,"type":"","file":""};
+  return list_file_after_check;
+
+  async function check_text_include_in_file(file, text) {
+    let is_text_found = { "status": false, "type": "", "file": "" };
     let workbook;
+
     if (file.name.endsWith(".xlsx")) {
-      workbook = XLSX.read(await file.arrayBuffer(), { type: "array",encoding: 'utf8' });
+      workbook = XLSX.read(await file.arrayBuffer(), { type: "array", encoding: 'utf8' });
       if (workbook) {
         for (const sheetName in workbook.Sheets) {
           const worksheet = workbook.Sheets[sheetName];
-          const usedRange = worksheet['!ref']; // Get used range (optional)
-
           for (const ref in worksheet) {
-            if (ref !== '!ref') { // Skip '!ref' property
+            if (ref !== '!ref') {
               const cell = worksheet[ref];
-              if (cell && cell.v) { // Check if cell has a value
-                if (cell.v) {
-                  const regex = new RegExp(text, 'i'); // Create a case-insensitive regex
-                  if (regex.test(cell.v.toString())) {
-                    is_text_found["status"] = true;
-                    is_text_found["type"] = "xlsx";
-                    is_text_found["file"] = template[text];
-                    break; // Stop iterating if text found
-                  }
+              if (cell && cell.v) {
+                const regex = new RegExp(text, 'i');
+                if (regex.test(cell.v.toString())) {
+                  is_text_found["status"] = true;
+                  is_text_found["type"] = "xlsx";
+                  is_text_found["file"] = template[text];
+                  break;
                 }
               }
             }
           }
+          if (is_text_found.status) break;
         }
-      
+      }
     }
-    
 
-    
-    }
     if (file.name.endsWith(".xml")) {
       const fileContent = await file.text();
-      const regex = new RegExp(text, 'i'); // Create a case-insensitive regex
+      const regex = new RegExp(text, 'i');
       if (regex.test(fileContent)) {
         is_text_found["status"] = true;
         is_text_found["type"] = "xml";
         is_text_found["file"] = template[text];
       }
     }
+
     return is_text_found;
   }
-
-  return list_file_after_check;
 };
--- src/app/(platform)/home/page.jsx
+++ src/app/(platform)/home/page.jsx
@@ -1,116 +1,326 @@
-'use client';
-import * as XLSX from 'xlsx/xlsx.mjs';
-import * as xml2js from "xml2js"; // Import xml2js library
-import exceljs from 'exceljs';
-import { toast } from 'react-hot-toast'; // Import toast from react-hot-toast
+'use client'
+import Image from "next/image"
+import closeicon from '@/assets/icon/close.svg'
+import noticicon from '@/assets/icon/notice.svg'
+import CustomUpload from "@/app/(platform)/_component/Upload/Upload"
+import { HandleListFile } from "../_component/HandleFile/HandleListFile"
+import { useEffect, useState } from "react"
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
-const key_for_process = {
-  "nhật ký chung": ["process_4", "process_3", "process_5", "process_6"],
-  "BẢNG CÂN ĐỐI TÀI KHOẢN": ["process_1", "process_3", "process_2"],
-  "thông tư 133": ["process_2"],
-  "thông tư 200": ["process_2"],
-  "báo cáo tài chính": ["process_4"],
-  "TỜ KHAI QUYẾT TOÁN THUẾ THU NHẬP DOANH NGHIỆP ": ["process_5"],
-  "TỜ KHAI THUẾ GIÁ TRỊ GIA TĂNG": ["process_6"],
-};
+import { Button, Radio, Modal } from "antd"
 
-const template = {
-  "nhật ký chung": "NKC",
-  "BẢNG CÂN ĐỐI TÀI KHOẢN": "CDPS",
-  "thông tư 133": "TT133",
-  "thông tư 200": "TT200",
-  "báo cáo tài chính": "BCTC",
-  "TỜ KHAI QUYẾT TOÁN THUẾ THU NHẬP DOANH NGHIỆP ": "QTTN",
-  "TỜ KHAI THUẾ GIÁ TRỊ GIA TĂNG": "TTGT",
-};
+import { PutObjectToS3 } from "../_component/Upload/UploadFileToS3"
 
-export const HandleListFile = async (userid, listfile) => {
-  const list_file_after_check = {
-    "listfile": [],
-    "process_1": [],
-    "process_2": [],
-    "process_3": [],
-    "process_4": [],
-    "process_5": [],
-    "process_6": []
-  };
+const Home = () => {
+    const [isCheck, setIsCheck] = useState(false)
+    const [isHasFile, setIsHasFile] = useState(true)
+    const [fileList, setFileList] = useState([]);
+    const [list_file_after_check, setList_file_after_check] = useState({});
+    const [status, setStatus] = useState({
+        "process_1": {
+            "CDPS": false,
+        },
+        "process_2": {
+            "CDPS": false,
+            "TT": false,
+        },
+        "process_3": {
+            "CDPS": false,
+            "NKC": false,
+        },
+        "process_4": {
+            "NKC": false,
+            "BCTC": false,
+        },
+        "process_5": {
+            "QTTN": false,
+            "NKC": false,
+        },
+        "process_6": {
+            "NKC": false,
+            "TTGT": false,
+        },
+    });
 
-  const baseUrl = `https://accountfolderpublic.s3.ap-southeast-1.amazonaws.com/${userid}/`;
+    const [previewVisible, setPreviewVisible] = useState(false);
+    const [previewImage, setPreviewImage] = useState('');
+    const [previewTitle, setPreviewTitle] = useState('');
 
-  for (const file of listfile) {
-    const fileurl = baseUrl + file.name;
-    list_file_after_check["listfile"].push(fileurl);
-  }
+    const userid = "12345678";
 
-  let foundTT133 = false;
-  let foundTT200 = false;
-
-  for (const file of listfile) {
-    for (const [key, list_process] of Object.entries(key_for_process)) {
-      const is_key_in_file = await check_text_include_in_file(file, key);
-      if (is_key_in_file["status"]) {
-        if (key === "thông tư 133") {
-          foundTT133 = true;
+    const processConditions = {
+        "process_1": { "CDPS": (type) => type === "CDPS" },
+        "process_2": {
+            "TT": (type) => type === "TT133" || type === "TT200",
+            "CDPS": (type) => type === "CDPS"
+        },
+        "process_3": {
+            "NKC": (type) => type === "NKC",
+            "CDPS": (type) => type === "CDPS"
+        },
+        "process_4": {
+            "NKC": (type) => type === "NKC",
+            "BCTC": (type) => type === "BCTC"
+        },
+        "process_5": {
+            "QTTN": (type) => type === "QTTN",
+            "NKC": (type) => type === "NKC"
+        },
+        "process_6": {
+            "NKC": (type) => type === "NKC",
+            "TTGT": (type) => type === "TTGT"
         }
-        if (key === "thông tư 200") {
-          foundTT200 = true;
+    };
+
+    const handlePreview = async (file) => {
+        if (!file.url && !file.preview) {
+            file.preview = await new Promise((resolve) => {
+                const reader = new FileReader();
+                reader.readAsDataURL(file.originFileObj);
+                reader.onload = () => resolve(reader.result);
+            });
         }
-        for (const process of list_process) {
-          const pre_data = {
-            "type": is_key_in_file["file"],
-            "name": file.name,
-          };
-          list_file_after_check[process].push(pre_data);
-        }
-      }
-    }
-  }
 
-  if (foundTT133 && foundTT200) {
-    toast.error("Chỉ được chọn một trong hai: Thông tư 133 hoặc Thông tư 200.");
-    return null; // or handle this case as per your application's requirements
-  }
+        setPreviewImage(file.url || file.preview);
+        setPreviewVisible(true);
+        setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
+    };
 
-  return list_file_after_check;
+    const handleCancel = () => setPreviewVisible(false);
 
-  async function check_text_include_in_file(file, text) {
-    let is_text_found = { "status": false, "type": "", "file": "" };
-    let workbook;
-
-    if (file.name.endsWith(".xlsx")) {
-      workbook = XLSX.read(await file.arrayBuffer(), { type: "array", encoding: 'utf8' });
-      if (workbook) {
-        for (const sheetName in workbook.Sheets) {
-          const worksheet = workbook.Sheets[sheetName];
-          for (const ref in worksheet) {
-            if (ref !== '!ref') {
-              const cell = worksheet[ref];
-              if (cell && cell.v) {
-                const regex = new RegExp(text, 'i');
-                if (regex.test(cell.v.toString())) {
-                  is_text_found["status"] = true;
-                  is_text_found["type"] = "xlsx";
-                  is_text_found["file"] = template[text];
-                  break;
-                }
-              }
+    function updatestatus(predata) {
+        const newStatus = { ...status };
+    
+        // Đặt tất cả các giá trị trong newStatus thành false trước khi cập nhật
+        for (const process in newStatus) {
+            for (const key in newStatus[process]) {
+                newStatus[process][key] = false;
             }
-          }
-          if (is_text_found.status) break;
         }
-      }
+    
+        // Cập nhật lại newStatus dựa trên predata
+        for (const process in predata) {
+            const data = predata[process];
+            const conditions = processConditions[process];
+    
+            for (const item of data) {
+                for (const key in newStatus[process]) {
+                    const condition = conditions[key];
+                    if (condition && condition(item.type)) {
+                        newStatus[process][key] = true;
+                    }
+                }
+            }
+        }
+        
+        console.log(newStatus);
+        setStatus(newStatus);
+    }
+    const checkForDuplicates = (fileList) => {
+        const fileNames = fileList.map(file => file.name);
+        const uniqueFileNames = new Set(fileNames);
+        return uniqueFileNames.size !== fileNames.length;
+    };
+    
+
+    useEffect(() => {
+        const handleFileList = async () => {
+            if (checkForDuplicates(fileList)) {
+                toast.error("Duplicate files detected. Please upload unique files.");
+                return;
+            }
+    
+            const predata = await HandleListFile(userid, fileList);
+            setList_file_after_check(predata);
+            updatestatus(predata);
+    
+            if (fileList.length > 0) {
+                setIsHasFile(true);
+            } else {
+                setIsHasFile(false);
+                setStatus({
+                    "process_1": { "CDPS": false },
+                    "process_2": { "CDPS": false, "TT": false },
+                    "process_3": { "CDPS": false, "NKC": false },
+                    "process_4": { "NKC": false, "BCTC": false },
+                    "process_5": { "QTTN": false, "NKC": false },
+                    "process_6": { "NKC": false, "TTGT": false }
+                });
+            }
+        };
+    
+        handleFileList();
+    }, [fileList]);
+    
+
+    function clear_list_file_after_check(list_file_after_check) {
+        const updatedData = { ...list_file_after_check };
+
+        for (const process in processConditions) {
+            const conditions = processConditions[process];
+            const dataForProcess = updatedData[process];
+
+            let allTypesPresent = true;
+            for (const key in conditions) {
+                const condition = conditions[key];
+                const typeExists = dataForProcess && dataForProcess.some(item => condition(item.type));
+                if (!typeExists) {
+                    allTypesPresent = false;
+                    break;
+                }
+            }
+
+            if (!allTypesPresent) {
+                delete updatedData[process];
+            }
+        }
+        return updatedData;
     }
 
-    if (file.name.endsWith(".xml")) {
-      const fileContent = await file.text();
-      const regex = new RegExp(text, 'i');
-      if (regex.test(fileContent)) {
-        is_text_found["status"] = true;
-        is_text_found["type"] = "xml";
-        is_text_found["file"] = template[text];
-      }
+    async function onHandleProcess() {
+        const status = await PutObjectToS3(fileList);
+        if (status) {
+            try {
+                const after_clear = clear_list_file_after_check(list_file_after_check);
+                const res = await axios.post('http://13.214.156.123/main_process/', after_clear);
+                if (res.status === 200) {
+                    console.log("Xử lí thành công");
+                    toast.success("Xử lí thành công");
+                } else {
+                    toast.error("Có lỗi xảy ra khi xử lí dữ liệu");
+                }
+            } catch (error) {
+                console.error(error);
+                toast.error("Có lỗi xảy ra khi xử lí dữ liệu");
+            }
+        }
     }
 
-    return is_text_found;
-  }
-};
+    const isAllChecked = (process) => {
+        return Object.values(status[process]).every(Boolean);
+    };
+    const handleClearAll = () => {
+        setFileList([]);
+    }
+    return (
+        <div className="p-[15px] bg-[#f7f7f7] w-full flex flex-col gap-y-[15px] bg-slate-100">
+            <div className="flex items-center bg-white p-4">
+                <Image src={noticicon} alt="" width={24} height={24} className="cursor-pointer" />
+                <p className="flex-1">Doanh thu và thuế GTGT trên Tổng cục Thuế được lấy từ các hoá đơn trên dữ liệu Tổng cục Thuế</p>
+                <Image src={closeicon} alt="" width={24} height={24} className="cursor-pointer" />
+            </div>
+            <div className="flex flex-row gap-x-[19px]">
+                {/* file */}
+                <div className="flex flex-col gap-y-[15px] w-3/4">
+                    <h3 className="font-bold leading-8 text-[23px]">Nhập tài liệu</h3>
+                    <div className="flex flex-col min-h-[875px] bg-white px-4 py-5">
+                        <div>
+                            <CustomUpload fileList={fileList} setFileList={setFileList} handlePreview={handlePreview} />
+                        </div>
+                        <div className="flex-grow flex flex-col gap-y-[15px] mt-[15px] overflow-auto">
+                            <Modal
+                                visible={previewVisible}
+                                title={previewTitle}
+                                footer={null}
+                                onCancel={handleCancel}
+                            >
+                                <img
+                                    alt="example"
+                                    style={{ width: '100%' }}
+                                    src={previewImage}
+                                />
+                            </Modal>
+                        </div>
+                        <div className="flex flex-row items-center justify-between pt-4 border-t-[1px]">
+                        <Button 
+                                className={fileList.length > 0 ? `opacity-100` : `opacity-40`} 
+                                type="primary" 
+                                danger 
+                                ghost 
+                                onClick={handleClearAll}
+                                disabled={fileList.length === 0}
+                            >
+                                Xóa tất cả
+                            </Button>
+                            <Button onClick={onHandleProcess} type="primary">Kiểm tra</Button>
+                        </div>
+                    </div>
+                </div>
+                {/* status */}
+                <div className="w-1/4 flex flex-col gap-y-[10px] mt-[47px]">
+                    <div className={`border-[1px] rounded-md flex flex-col gap-y-2 p-[10px] ${isAllChecked('process_1') ? 'bg-checked' : ''}`}>
+                        <div className="div">1.Đối chiếu số dư tổng hợp trên bảng CDPS</div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_1.CDPS}></Radio>
+                            <span>BẢNG CÂN ĐỐI TÀI KHOẢN(CDPS)</span>
+                        </div>
+                    </div>
+
+                    <div className={`border-[1px] rounded-md flex flex-col gap-y-2 p-[10px] ${isAllChecked('process_2') ? 'bg-checked' : ''}`}>
+                        <div className="div">2.Kiểm tra hệ thống tài khoản của doanh nghiệp</div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_2.CDPS}></Radio>
+                            <span>BẢNG CÂN ĐỐI TÀI KHOẢN(CDPS)</span>
+                        </div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_2.TT}></Radio>
+                            <span>DANH SÁCH THÔNG TƯ(TT)</span>
+                        </div>
+                    </div>
+
+                    <div className={`border-[1px] rounded-md flex flex-col gap-y-2 p-[10px] ${isAllChecked('process_3') ? 'bg-checked' : ''}`}>
+                        <div className="div">3. Kiểm tra tổng số phát sinh trên CDPS và NKC</div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_3.CDPS}></Radio>
+                            <span>BẢNG CÂN ĐỐI TÀI KHOẢN(CDPS)</span>
+                        </div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_3.NKC}></Radio>
+                            <span>Sổ nhật ký chung(NKC)</span>
+                        </div>
+                    </div>
+
+                    <div className={`border-[1px] rounded-md flex flex-col gap-y-2 p-[10px] ${isAllChecked('process_4') ? 'bg-checked' : ''}`}>
+                        <div className="div">4.So sánh số liệu doanh thu (NKC vs BCTC)</div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_4.NKC}></Radio>
+                            <span>Sổ nhật ký chung(NKC)</span>
+                        </div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_4.BCTC}></Radio>
+                            <span>Báo cáo tài chính(BCTC)</span>
+                        </div>
+                    </div>
+
+                    <div className={`border-[1px] rounded-md flex flex-col gap-y-2 p-[10px] ${isAllChecked('process_5') ? 'bg-checked' : ''}`}>
+                        <div className="div">5. So sánh số liệu doanh thu (Sổ NKC và TNDN)</div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_5.NKC}></Radio>
+                            <span>Sổ nhật ký chung(NKC)</span>
+                        </div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_5.QTTN}></Radio>
+                            <span>TỜ KHAI QUYẾT TOÁN THUẾ THU NHẬP DOANH NGHIỆP(QTTN)</span>
+                        </div>
+                    </div>
+
+                    <div className={`border-[1px] rounded-md flex flex-col gap-y-2 p-[10px] ${isAllChecked('process_6') ? 'bg-checked' : ''}`}>
+                        <div className="div">6.Đối chiếu doanh thu  (Sổ NKC và TTGT)</div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_6.NKC}></Radio>
+                            <span>Sổ nhật ký chung(NKC)</span>
+                        </div>
+                        <div className="flex items-center">
+                            <Radio checked={status.process_6.TTGT}></Radio>
+                            <span>TỜ KHAI THUẾ GIÁ TRỊ GIA TĂNG(TTGT)</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Home
